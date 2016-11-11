73300cb91c88e997c1248febc3c5d609453bd2d7
Pankaj Patel <patel.pankaj@live.in>
2016-11-11 07:17:25 +0530

    compare

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+var css = require('css');
 var cssParse = require('css-parse');
+// var obj = css.parse('body { font-size: 12px; }', options);
+// css.stringify(obj, options);
+
+fs = require('fs');
+fs.readFile('test/a.css', 'utf8', function (err, data) {
+	if (err) {
+		return console.log(err);
+  	}
+	fs.readFile('test/b.css', 'utf8', function (e,d) {
+		if (err) {
+			return console.log(err);
+		}
+		var a = css.parse(data);
+		var b = css.parse(d);
+		console.log(JSON.stringify( a ), JSON.stringify( b ) )
+	});
+});
